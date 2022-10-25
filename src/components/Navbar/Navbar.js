e373b6b232b56ad4e6a7561722200d4c1fb96e82
GHSoyad <ghsoyad@gmail.com>
2022-10-25 12:57:22 +0600

    Added navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            This is Navbar
+        <div className="navbar bg-base-100 container mx-auto max-w-screen-xl">
+            <div className="flex-1">
+                <Link to='/'><img src="logo.png" alt="" className='max-w-[260px] rounded' /></Link>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal p-0 gap-2">
+                    <li><NavLink to='/courses'>Courses</NavLink></li>
+                    <li><NavLink to='/faq'>FAQ</NavLink></li>
+                    <li><NavLink to='/login'>Login</NavLink></li>
+                    <li><NavLink to='/register'>Register</NavLink></li>
+                </ul>
+            </div>
         </div>
     );
 };
